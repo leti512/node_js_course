23d6f42a6c21938ac5b909daa0a8e371cdd68bb9
Leticia Diaz <leticiadiaz512@gmail.com>
2024-04-13 23:53:48 -0600

    :test_tube: FEAT: example test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,15 @@
 describe('App', () => {
     test('should be true', () => {
-        expect(true).toBe(true);
+
+        //1. Arrange
+        const num1 = 10;
+        const num2 = 20;
+
+        //2. Act
+        const result = num1 + num2;
+
+        //3. Assert
+        expect(result).toBe(35);
+        //expect(true).toBe(true);
     });
 })
